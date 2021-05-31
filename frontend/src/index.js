ca1ffeea8d7725ed19b2e4a94d5b9d27df561907
Eva <yi.zheng1709@gmail.com>
2021-05-31 18:56:59 -0400

    adds Provider to index.js; installs redux-thunk

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import animesReducer from './reducers/animesReducer'
+import thunk from 'redux-thunk'
+
+const store = createStore(animesReducer)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+)
