45c625ee37783ae12ad67acbd52f099bb9e21500
Eva <yi.zheng1709@gmail.com>
2021-05-31 20:36:52 -0400

    defines index, create, and comment_params in commentscontroller in backend

--- backend/anime_api/app/controllers/comments_controller.rb
+++ backend/anime_api/app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+
+    def index 
+        if params["anime_id"]
+            comments = Comment.where("anime_id = ?", params["anime_id"])
+            render json: comments, only: [:anime_id, :content, :name]
+        end
+    end
+
+    def create 
+        comment = Comment.new(comment_params)
+        if comment.save 
+            render json: comment, only: [:anime_id, :content, :name]
+        end
+    end
+
+    private 
+
+    def comment_params 
+        params.require(:comment).permit(:anime_id, :name, :content)
+    end
 end
--- backend/anime_api/config/routes.rb
+++ backend/anime_api/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :animes
+  resources :animes do 
+    resources :comments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
