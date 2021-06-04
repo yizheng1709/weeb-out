a1f62a802f565d834a49b4a756c6174358a84775
Eva <yi.zheng1709@gmail.com>
2021-06-04 11:38:13 -0400

    removes debugging console log statement

--- frontend/src/reducers/animesReducer.js
+++ frontend/src/reducers/animesReducer.js
@@ -1,5 +1,4 @@
 export default function animesReducer (state = {animes: [] }, action) {
-    console.log(state, "in animes reducers")
     //reducer is automaticallly passed the previous state
     //the initial state is only needed to prevent the error from the initial render
     switch(action.type){
